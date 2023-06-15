600a09191c3022173722a6ecf15ff12fc451c07c
Felixxx <felixxx@vivaldi.net>
2023-06-15 10:42:13 +0300

    change README

--- main.cpp
+++ main.cpp
@@ -1,27 +1,37 @@
 #include <iostream>
+#include <vector>
 #include <fstream>
 
 int main() {
-    int size = 0;
+    int size;
     std::ifstream fin("/home/felixxx/Clion/HW01/in.txt");
     std::ofstream fileout("/home/felixxx/Clion/HW01/out.txt");
 
 
     if(fin.is_open()) {
-        int* mas = new int [size];
         while (!fin.eof()) {
+            int* mas = new int [size];
             fin >> size;
             fileout << size;
+            std::rotate (size, size-1);
             std::cout << size << ' '<< '\n';
             fileout << '\n';
-            for (int i = 0; i < size; i++) {
-                fin >> mas[i];
-                    fileout << mas[i] << ' ';
-                    std::cout << mas[i] << ' ';
 
+            for (int i = 0; i < size; i++)
+            {
+                fin >> mas[i];
+            }
+
+            std:: reverse (mas, mas+size);
+
+            for (int i = 0; i < size; i++) {
+                fileout << mas[i] << ' ';
+                std::cout << mas[i] << ' ';
             }
             std:: cout << '\n';
             fileout << '\n';
+
+            delete[] mas;
         }
     }
     else{
