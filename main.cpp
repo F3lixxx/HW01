432c0f0920b6d877e6112c49f327dc53ce6d4f15
yenox <felixxx@vivaldi.net>
2023-06-15 14:48:14 +0300

    change main code

--- main.cpp
+++ main.cpp
@@ -1,42 +1,48 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <algorithm>
 
-int main() {
-    int size;
-    std::ifstream fin("/home/felixxx/Clion/HW01/in.txt");
-    std::ofstream fileout("/home/felixxx/Clion/HW01/out.txt");
+void file(std::string in){
+    //std::ifstream fin("/home/felixxx/Clion/HW01/in.txt"); //это в убунту
+    std::ifstream fin(in); //винда
+    //std::ofstream fileout("/home/felixxx/Clion/HW01/out.txt"); //это в убунту
+    std::ofstream fileout("out.txt"); //винда
 
-
-    if(fin.is_open()) {
+    if(fin.is_open() && fileout.is_open()) {
+        int size;
         while (!fin.eof()) {
-            int* mas = new int [size];
             fin >> size;
             fileout << size;
-            std::rotate (size, size-1);
-            std::cout << size << ' '<< '\n';
+            std::cout << size << ' ' << '\n';
             fileout << '\n';
 
-            for (int i = 0; i < size; i++)
-            {
+            int *mas = new int[size];
+            for (int i = size-1; i >= 0 ; i--) {
                 fin >> mas[i];
             }
 
-            std:: reverse (mas, mas+size);
 
-            for (int i = 0; i < size; i++) {
+            for (int i = 0; i < size; i++){
                 fileout << mas[i] << ' ';
                 std::cout << mas[i] << ' ';
             }
-            std:: cout << '\n';
+
+            std::cout << '\n';
             fileout << '\n';
 
             delete[] mas;
         }
-    }
-    else{
+    } else{
         std:: cout << "File is not open!";
     }
-        fin.close();
+    fin.close();
+    fileout.close();
+}
+
+int main() {
+
+    file("in.txt");
+
     return 0;
 }
